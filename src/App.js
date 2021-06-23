f4ae79ef2e62a9bc5930f1d0087c144d37fdb903
jfilipecpereira <profilipejcp@gmail.com>
2021-06-23 11:03:29 +0100

    folder change
    folder change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Main from "./Components/Pages/Main"
 import Products from "./Components/Pages/Products"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+
 function App() {
 	return (
 		<ClientProvider>
